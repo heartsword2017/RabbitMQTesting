0c849a0ced0964447f0c49bee22fa6c9d9e77284
Xinjian Yao <xinjianyao@carfax.com>
2018-07-06 14:32:16 -0500

    practising with exchange of FANOUT type by Xinjian

---
+++ src/main/java/PublishSubscribeStyle/ExchangeTestProducer.java
@@ -0,0 +1,25 @@
+package PublishSubscribeStyle;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ExchangeTestProducer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare("logs","fanout");
+        String message = "I am just a log info, check it out Xinjian, you just got a log!!!";
+        channel.basicPublish("logs","routingKeyIsNotNeededForFanOut",MessageProperties.PERSISTENT_TEXT_PLAIN,message.getBytes());
+        System.out.println("Xinjian!!!Congratz, your message has been sent!! What an amazing stuff");
+        channel.close();
+        connection.close();
+
+    }
+}
---
+++ src/main/java/PublishSubscribeStyle/ExchangeTestReceiver.java
@@ -0,0 +1,37 @@
+package PublishSubscribeStyle;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ExchangeTestReceiver {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare("logs","fanout");
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName,"logs","Again,No need for a routing key of fanout type exchange!");
+        System.out.println("Hey, I am just a random worker");
+
+        com.rabbitmq.client.Consumer myConsumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body){
+                String message = new String(body);
+                System.out.println("I got the following logs\n"+message);
+                try {
+                    channel.basicAck(envelope.getDeliveryTag(),false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        channel.basicConsume(queueName,false,myConsumer);
+
+        channel.basicQos(1);
+    }
+}
